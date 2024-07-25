7aa686a7ffca98a9220a74c77ff5cd0ab8a11562
qwahzi <qwahzi@gmail.com>
2024-07-25 07:57:44 -0500

    Update buckets.py for V27

--- buckets.py
+++ buckets.py
@@ -1,15 +1,15 @@
-# Python code to print Nano prioritization buckets
-# Based on: https://github.com/clemahieu/nano-node/blob/d718d697796085c43ca0c575f85f132bb46d3028/nano/node/prioritization.cpp#L50
+# Python code to print Nano prioritization buckets (V27)
+# Based on: https://github.com/nanocurrency/nano-node/pull/4661/files
 
 # Column headers:
-print ( f'{"Bucket Number":<15}' + f'{"Bucket Region":<15}' + f'{"Bucket Range (Nano)":<45}' + f'{"Bucket Range (Raw)":<30}' )
+print ( f'{"Bucket Number":<15}' + f'{"Bucket Region":<15}' + f'{"Bucket Range (Nano)":<33}' + f'{"Bucket Range (Raw)":<30}' )
 
 # Initialize lists
 minimums_list = []
 maximums_list = []
 exponent_list = []
 
-def build_region (begin, end, count):
+def build_region(begin, end, count):
     # Determine how wide a single sub-region (bucket) is:
     width = ((1<<end) - (1<<begin)) / count
     
@@ -22,23 +22,30 @@ def build_region (begin, end, count):
         i += 1
 
 minimums_list.append(0)
-maximums_list.append((1<<88))
-exponent_list.append("0-2^88")
-build_region (88, 92, 2)
-build_region (92, 96, 4)
-build_region (96, 100, 8)
-build_region (100, 104, 16)
-build_region (104, 108, 16)
-build_region (108, 112, 8)
-build_region (112, 116, 4)
-build_region (116, 120, 2)
-build_region (120, 128, 1)
+maximums_list.append((1<<79))
+exponent_list.append("0-2^79")
+build_region(79, 88, 1)
+build_region(88, 92, 2)
+build_region(92, 96, 4)
+build_region(96, 100, 8)
+build_region(100, 104, 16)
+build_region(104, 108, 16)
+build_region(108, 112, 8)
+build_region(112, 116, 4)
+build_region(116, 120, 2)
+build_region(120, 128, 1)
 
 x = 0
 while x < len(minimums_list):
-    print(f'{"Bucket " + str(x):<15}' \
+    #Get the minimum range in float, convert to string, truncate to 15 chars
+    minimum_float_truncated = str(f'{minimums_list[x]/(10**30):,.17f}')[:15]
+
+    #Get the maximum range in float, convert to string, truncate to 15 chars
+    maximum_float_truncated = str(f'{maximums_list[x]/(10**30):,.17f}')[:15]
+    
+    print(f'{str(x):<15}' \
             f'{exponent_list[x]:<14}', \
-            f"{f'{minimums_list[x]/(10**30):,}' + '-' + f'{(maximums_list[x])/(10**30):,}':<44}", \
+            f"{minimum_float_truncated + '-' + maximum_float_truncated:<32}", \
             str(int(minimums_list[x])) + "-" + str(int((maximums_list[x]))) )
     x += 1
 
